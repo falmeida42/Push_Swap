b614d3ef51f3d706a042c6792082b2de63bf4f9a
Flavio Almeida <falmeida@c2r3s1.42lisboa.com>
2021-07-01 20:43:39 +0100

    Changes Push_Swap 01/07/2021

--- pilhaAB.cpp
+++ pilhaAB.cpp
@@ -8,37 +8,102 @@ typedef struct t_list
 }t_list;
 
 
-void no_esquerda_a(t_list *primeirono, int valor)
+void no_esquerda(t_list **primeirono, int valor)
 {
     t_list *newno = (t_list *)malloc(sizeof(t_list));
-    t_list *old_head = primeirono->proximo;
     
-    primeirono->proximo = newno;
-    newno->proximo = old_head;
+    newno->proximo = *primeirono;
     newno->valor = valor;
+    *primeirono = newno;
 }
-t_list *no_esquerda_b(t_list *primeironob, int valor)
+void delet_esquerda(t_list **primeirono)
 {
+    t_list *newno = *primeirono;
+    *primeirono = (*primeirono)->proximo;
+    free(newno);
+
+}
+
+void no_direita(t_list *primeirono, int valor)
+{
+    t_list *aux;
     t_list *newno = (t_list *)malloc(sizeof(t_list));
+
+    aux = primeirono;
+    while (aux->proximo != NULL)
+    {
+        aux = aux->proximo;
+    }
+    aux->proximo = newno;
     newno->valor = valor;
-    newno->proximo = primeironob;
+    newno->proximo = NULL;
+
+}
+void delet_direita(t_list *primeirono)
+{
+    t_list *aux;
+
+    aux = primeirono;
+    while (aux->proximo->proximo != NULL)
+    {
+        aux = aux->proximo;
+    }
+    free(aux->proximo);
+    aux->proximo = NULL;
 }
 
-t_list *delet_esquerda_a(t_list *primeirono)
+void swap(t_list *primeirono)
 {
-    t_list *newno = primeirono;
-    primeirono = primeirono->proximo;
-    free(newno);
-    
-    return (primeirono);
-}
-t_list *delet_esquerda_b(t_list *primeironob)
-{
-    t_list *newno = primeironob;
-    primeironob = primeironob->proximo;
-    free(newno);
+    int valor;
 
-    return (primeironob);
+    if(!primeirono || !primeirono->proximo)
+        return ;
+    valor = primeirono->valor;
+    primeirono->valor = primeirono->proximo->valor;
+    primeirono->proximo->valor = valor;
+}
+void swap_a_b(t_list *primeirono, t_list *primeironob)
+{
+    swap(primeirono);
+    swap(primeironob);
+}
+
+void push_a(t_list **primeirono, t_list **primeironob)
+{
+    no_esquerda(primeironob, (*primeirono)->valor);
+    delet_esquerda(primeirono);
+}
+void push_b(t_list **primeirono, t_list **primeironob)
+{
+    no_esquerda(primeirono, (*primeironob)->valor);
+    delet_esquerda(primeironob);
+}
+
+void rotate(t_list **primeirono)
+{
+    no_direita((*primeirono), (*primeirono)->valor);
+    delet_esquerda(primeirono);
+}
+void rotate_ab(t_list **primeirono, t_list **primeironob)
+{
+    rotate(primeirono);
+    rotate(primeironob);
+}
+
+void reverse_rotate(t_list **primeirono)
+{
+    t_list *aux;
+
+    aux = *primeirono;
+    while (aux->proximo != NULL)
+        aux = aux->proximo;
+    no_esquerda(primeirono, aux->valor);
+    delet_direita(*primeirono);
+}
+void reverse_rotate_ab(t_list **primeirono, t_list **primeironob)
+{
+    reverse_rotate(primeirono);
+    reverse_rotate(primeironob);
 }
 
 void show_list(t_list *primeirono)
@@ -67,32 +132,31 @@ void show_list_b(t_list *primeironob)
     }
     printf("\n");
 }
-void swap_a(t_list *primeirono)
-{
-    int valor;
 
-    if(!primeirono || !primeirono->proximo)
-        return ;
-    valor = primeirono->valor;
-    primeirono->valor = primeirono->proximo->valor;
-    primeirono->proximo->valor = valor;
-}
-void swap_b(t_list *primeirono)
+void sorted(t_list *primeirono, t_list *primeironob)
 {
-    int valor;
+    t_list *aux;
+    t_list *auxb;
+    aux = primeirono;
+    auxb = primeironob;
 
-    if(!primeirono || !primeirono->proximo)
-        return ;
-    valor = primeirono->valor;
-    primeirono->valor = primeirono->proximo->valor;
-    primeirono->proximo->valor = valor;
+    /*
+    if (!NULL)
+    {
+        printf("ERROR");
+    }
+    */
+    while (aux->proximo != NULL)
+    {
+        if (aux->valor > aux->proximo->valor)
+        {
+            printf("Nao ordenado\n");
+            return ;
+        }
+        aux = aux->proximo;
+    }
+    printf("Ordenado\n");
 }
-void swap_a_b(t_list *primeirono, t_list *primeironob)
-{
-    swap_a(primeirono);
-    swap_b(primeironob);
-}
-
 
 int main(void)
 {   
@@ -124,6 +188,7 @@ int main(void)
     terceironob->valor = 80;
     terceironob->proximo = NULL;
     
+    sorted(primeirono, primeironob);
     show_list(primeirono);
     show_list_b(primeironob);
     return (0);
